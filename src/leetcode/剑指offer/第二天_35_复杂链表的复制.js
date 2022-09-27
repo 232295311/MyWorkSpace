910a0ffa9aab15d435a97181258ffb9c1ea41b86
232295311@qq.com <232295311@qq.com>
2022-09-27 19:12:36 +0800

    feat: complete second day

---
+++ src/leetcode/剑指offer/第二天_35_复杂链表的复制.js
@@ -0,0 +1,38 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function (head) {
+  if (!head) {
+    return head;
+  }
+  let curr = head; //head不动，因为之后要重新遍历一遍
+
+  const newListNode = new Node();
+  let temp = newListNode; //newListNode不动，因为之后要重新遍历一遍
+
+  const map = new Map();
+  while (curr != null) {
+    temp.val = curr.val;
+    temp.next = curr.next ? new Node() : null;
+    map.set(curr, temp);
+    curr = curr.next;
+    temp = temp.next;
+  }
+  temp = newListNode; //回到起点
+  while (head != null) {
+    temp.random = head.random ? map.get(head.random) : null;
+    head = head.next;
+    temp = temp.next;
+  }
+  return newListNode;
+};
