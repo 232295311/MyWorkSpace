fc7237cbf631843a7df49a5afce18d6b70fc3da9
232295311@qq.com <232295311@qq.com>
2023-02-04 12:45:23 +0800

    feat: nothing

---
+++ src/leetcode/排序算法/冒泡排序.js
@@ -0,0 +1,12 @@
+/*
+ * @Description:
+ * @Author: Tulandbla
+ * @GitHubUserName: 232295311@qq.com
+ */
+const bubbleSort = (nums) => {
+  let numsLength = nums.length;
+  let temp;
+  for (let i = 0; i < numsLength; i++) {
+    for (let j = i; j < nums.length; j++) {}
+  }
+};
